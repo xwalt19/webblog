a06bd5cf5d802fcf9479104a23dbd793f700d4cb
RIFAL AZHAR PERMANA <permanarifal269@gmail.com>
2025-07-02 14:48:43 +0700

    [dyad] wrote 1 file(s)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import HeroSection from "@/components/HeroSection";
 import LatestBlogPosts from "@/components/LatestBlogPosts";
 import WhyProCodeCG from "@/components/WhyProCodeCG";
-import CallToActionSection from "@/components/CallToActionSection"; // Import komponen baru
+import CallToActionSection from "@/components/CallToActionSection";
+import FeaturedCourses from "@/components/FeaturedCourses"; // Import komponen FeaturedCourses
 
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <HeroSection />
       <WhyProCodeCG />
-      <CallToActionSection /> {/* Komponen baru ditambahkan di sini */}
+      <FeaturedCourses /> {/* Komponen FeaturedCourses ditambahkan di sini */}
+      <CallToActionSection />
       <LatestBlogPosts />
       {/* Bagian lain untuk homepage dapat ditambahkan di sini */}
     </div>
