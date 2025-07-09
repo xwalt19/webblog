e97588be5c08255bbac84f8b107757e3c8ceb092
RIFAL AZHAR PERMANA <permanarifal269@gmail.com>
2025-07-09 13:42:49 +0700

    [dyad] Memperbaiki posisi dropdown menu 'Media' di navigasi - wrote 1 file(s)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -104,7 +104,7 @@ const Layout: React.FC = () => {
               {/* New Media Navigation Item */}
               <NavigationMenuItem>
                 <NavigationMenuTrigger className="bg-background text-foreground hover:text-primary data-[active]:bg-accent data-[state=open]:bg-accent data-[active]:text-accent-foreground data-[state=open]:text-accent-foreground">Media</NavigationMenuTrigger>
-                <NavigationMenuContent className="bg-background text-foreground border-border md:left-auto md:right-0"> {/* Added md:left-auto md:right-0 */}
+                <NavigationMenuContent className="bg-background text-foreground border-border"> {/* Removed md:left-auto md:right-0 */}
                   <ul className="grid gap-3 p-4 md:w-[200px]">
                     <li>
                       <NavigationMenuLink asChild>
