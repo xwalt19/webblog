43c389dc1428f53ac1af7a0929bfdedccf1ba2c8
RIFAL AZHAR PERMANA <permanarifal269@gmail.com>
2025-07-28 15:44:56 +0700

    [dyad] Menghapus TooltipProvider dari App.tsx untuk mengatasi kesalahan 'React.Children.only'. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+// import { TooltipProvider } from "@/components/ui/tooltip"; // Dihapus
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
@@ -21,52 +21,49 @@ import CalendarPage from "./pages/info/CalendarPage";
 import UploadContent from "./pages/UploadContent";
 import ContentList from "./pages/ContentList";
 import MigrateBlogPosts from "./pages/MigrateBlogPosts";
-import UploadBlogPost from "./pages/UploadBlogPost"; // Import halaman UploadBlogPost
+import UploadBlogPost from "./pages/UploadBlogPost";
 import NotFound from "./pages/NotFound";
-import Login from "./pages/Login"; // Import halaman Login
-import { SessionProvider } from "./components/SessionProvider"; // Import SessionProvider
-import React from "react"; // Import React untuk React.Fragment
+import Login from "./pages/Login";
+import { SessionProvider } from "./components/SessionProvider";
+import React from "react";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      {/* Membungkus Toaster, Sonner, dan BrowserRouter dalam satu React.Fragment */}
-      <React.Fragment>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <SessionProvider> {/* Wrap the entire app with SessionProvider */}
-            <Routes>
-              <Route path="/login" element={<Login />} /> {/* Add login route outside Layout */}
-              <Route path="/" element={<Layout />}>
-                <Route index element={<Home />} />
-                <Route path="posts/:id" element={<PostDetail />} />
-                <Route path="blog" element={<BlogPage />} />
-                <Route path="about" element={<AboutPage />} />
-                <Route path="archives" element={<Archives />} />
-                <Route path="info/regular-events-classes" element={<RegularEventsClasses />} />
-                <Route path="info/camps" element={<Camps />} />
-                <Route path="info/training" element={<Training />} />
-                <Route path="info/programs" element={<ProgramsPage />} />
-                <Route path="contact-us" element={<ContactUs />} />
-                <Route path="partners" element={<Partners />} />
-                <Route path="media/youtube" element={<YouTubePage />} />
-                <Route path="media/tiktok" element={<TikTokPage />} />
-                <Route path="info/calendar" element={<CalendarPage />} />
-                <Route path="upload-content" element={<UploadContent />} />
-                <Route path="content" element={<ContentList />} />
-                <Route path="migrate-blog-posts" element={<MigrateBlogPosts />} />
-                <Route path="upload-blog-post" element={<UploadBlogPost />} /> {/* Rute baru untuk upload blog post */}
-                {/* TAMBAHKAN SEMUA RUTE KUSTOM DI ATAS RUTE CATCH-ALL "*" */}
-                <Route path="*" element={<NotFound />} />
-              </Route>
-            </Routes>
-          </SessionProvider>
-        </BrowserRouter>
-      </React.Fragment>
-    </TooltipProvider>
+    {/* TooltipProvider telah dihapus */}
+    <React.Fragment>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <SessionProvider>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="posts/:id" element={<PostDetail />} />
+              <Route path="blog" element={<BlogPage />} />
+              <Route path="about" element={<AboutPage />} />
+              <Route path="archives" element={<Archives />} />
+              <Route path="info/regular-events-classes" element={<RegularEventsClasses />} />
+              <Route path="info/camps" element={<Camps />} />
+              <Route path="info/training" element={<Training />} />
+              <Route path="info/programs" element={<ProgramsPage />} />
+              <Route path="contact-us" element={<ContactUs />} />
+              <Route path="partners" element={<Partners />} />
+              <Route path="media/youtube" element={<YouTubePage />} />
+              <Route path="media/tiktok" element={<TikTokPage />} />
+              <Route path="info/calendar" element={<CalendarPage />} />
+              <Route path="upload-content" element={<UploadContent />} />
+              <Route path="content" element={<ContentList />} />
+              <Route path="migrate-blog-posts" element={<MigrateBlogPosts />} />
+              <Route path="upload-blog-post" element={<UploadBlogPost />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </SessionProvider>
+      </BrowserRouter>
+    </React.Fragment>
   </QueryClientProvider>
 );
 
