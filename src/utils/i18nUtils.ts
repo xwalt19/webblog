c4ba2aaf686807c200578bd4b11f8e2bcea35eb1
RIFAL AZHAR PERMANA <permanarifal269@gmail.com>
2025-07-29 11:44:06 +0700

    [dyad] Memperbaiki tampilan tag di dropdown dan mengonfirmasi tampilan tanggal/waktu lengkap. - wrote 1 file(s)

--- src/utils/i18nUtils.ts
+++ src/utils/i18nUtils.ts
@@ -4,8 +4,13 @@ export const useTranslatedTag = () => {
   const { t } = useTranslation();
 
   const getTranslatedTag = (tagKey: string) => {
-    // Remove 'archives.tag ' prefix if it exists
-    const cleanedTagKey = tagKey.startsWith('archives.tag ') ? tagKey.substring('archives.tag '.length) : tagKey;
+    // Remove known prefixes if they exist
+    let cleanedTagKey = tagKey;
+    if (cleanedTagKey.startsWith('archives.tag ')) {
+      cleanedTagKey = cleanedTagKey.substring('archives.tag '.length);
+    } else if (cleanedTagKey.startsWith('blog posts.tag ')) {
+      cleanedTagKey = cleanedTagKey.substring('blog posts.tag '.length);
+    }
     // Translate the cleaned tag key
     return t(cleanedTagKey);
   };
@@ -15,5 +20,11 @@ export const useTranslatedTag = () => {
 
 // This function is for processing tags before saving to DB or for generating filter options
 export const cleanTagForStorage = (tag: string) => {
-  return tag.startsWith('archives.tag ') ? tag.substring('archives.tag '.length) : tag;
+  let cleanedTag = tag;
+  if (cleanedTag.startsWith('archives.tag ')) {
+    cleanedTag = cleanedTag.substring('archives.tag '.length);
+  } else if (cleanedTag.startsWith('blog posts.tag ')) {
+    cleanedTag = cleanedTag.substring('blog posts.tag '.length);
+  }
+  return cleanedTag;
 };
