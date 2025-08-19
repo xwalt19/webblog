fe884553bd68daeb6b3775ab1e1afec5a28824eb
RIFAL AZHAR PERMANA <permanarifal269@gmail.com>
2025-08-19 15:02:32 +0700

    [dyad] Mengoreksi jalur gambar logo DyCode - wrote 1 file(s) + extra files edited outside of Dyad

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -10,35 +10,35 @@ export const dummyPartners: Partner[] = [
   {
     id: "p1",
     nameKey: "dycode partner name",
-    logoUrl: "/assets/dycode-logo.png", // Mengoreksi jalur
+    logoUrl: "/assets/DyCode.png", // Mengoreksi jalur dan nama file
     descriptionKey: "dycode partner description",
     type: "corporate",
   },
   {
     id: "p2",
     nameKey: "codeintech partner name",
-    logoUrl: "/assets/CodeinTech.jpg", // Mengoreksi jalur
+    logoUrl: "/assets/CodeinTech.jpg",
     descriptionKey: "codeintech partner description",
     type: "community",
   },
   {
     id: "p3",
     nameKey: "telkom university partner name",
-    logoUrl: "/assets/Telkom University.png", // Mengoreksi jalur
+    logoUrl: "/assets/Telkom University.png",
     descriptionKey: "telkom university partner description",
     type: "educational",
   },
   {
     id: "p4",
     nameKey: "itb partner name",
-    logoUrl: "/assets/Institut Teknologi Bandung.webp", // Mengoreksi jalur
+    logoUrl: "/assets/Institut Teknologi Bandung.webp",
     descriptionKey: "itb partner description",
     type: "educational",
   },
   {
     id: "p5",
     nameKey: "startup bandung partner name",
-    logoUrl: "/assets/Startup Bandung.png", // Mengoreksi jalur
+    logoUrl: "/assets/Startup Bandung.png",
     descriptionKey: "startup bandung partner description",
     type: "community",
   },
